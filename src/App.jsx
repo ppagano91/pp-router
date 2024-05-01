6ae57df79416cf9e38eb78455f0b00b28ae547fd
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-01 13:59:35 -0300

    Probando paquete npm pp-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
 import { Suspense, lazy } from 'react'
 
 // import HomePage from './pages/Home'
-const LazyHomePage = lazy(()=>import('./pages/Home.jsx'))
+// const LazyHomePage = lazy(()=>import('./pages/Home.jsx'))
 // import AboutPage from './pages/About'
-const LazyAboutPage = lazy(()=>import('./pages/About.jsx'))
-import Router from './components/Router'
-import Page404 from './pages/Page404'
-import SearchPage from './pages/Search'
+// const LazyAboutPage = lazy(()=>import('./pages/About.jsx'))
+// import Router from './components/Router'
+// import Page404 from './pages/Page404'
+// import SearchPage from './pages/Search'
 import './App.css'
-import Route from './components/Route'
+// import Route from './components/Route'
 
-const routes = [
-  // {
-  //   path:'/',
-  //   Component: HomePage
-  // },
-  {
-    path:'/:lang/about',
-    Component: LazyAboutPage
-  },
-  {
-    path:'/search/:query',
-    Component: SearchPage
-  }
-]
+import {Router, Route} from 'pp-router'
+
+// const routes = [
+//   // {
+//   //   path:'/',
+//   //   Component: HomePage
+//   // },
+//   {
+//     path:'/:lang/about',
+//     Component: LazyAboutPage
+//   },
+//   {
+//     path:'/search/:query',
+//     Component: SearchPage
+//   }
+// ]
 
 function App() {  
   
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
-        <Router routes={routes} defaultComponent={Page404}>
-          <Route path='/' Component={LazyHomePage}/>
-          <Route path='/about' Component={LazyAboutPage}/>        
+        <Router>
+          <Route path='/' Component={()=><h1>HOLA MUNDO!</h1>}/>
+          <Route path='/about' Component={()=><h1>HOLA ABOUT</h1>}/>        
         </Router>
       </Suspense>
     </main>
