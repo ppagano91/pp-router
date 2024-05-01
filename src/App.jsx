d880c2996cd97d4f12494a56dbd2d173be7d9b76
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-01 10:58:00 -0300

    Agrego i18n a la página About

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
+import { Suspense, lazy } from 'react'
+
+// import HomePage from './pages/Home'
+const LazyHomePage = lazy(()=>import('./pages/Home.jsx'))
+// import AboutPage from './pages/About'
+const LazyAboutPage = lazy(()=>import('./pages/About.jsx'))
 import Router from './components/Router'
 import Page404 from './pages/Page404'
 import SearchPage from './pages/Search'
@@ -11,25 +15,26 @@ const routes = [
   //   path:'/',
   //   Component: HomePage
   // },
-  // {
-  //   path:'/about',
-  //   Component: AboutPage
-  // },
+  {
+    path:'/:lang/about',
+    Component: LazyAboutPage
+  },
   {
     path:'/search/:query',
     Component: SearchPage
   }
 ]
 
-function App() {
-  
+function App() {  
   
   return (
     <main>
-      <Router routes={routes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage}/>
-        <Route path='/about' Component={AboutPage}/>        
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={routes} defaultComponent={Page404}>
+          <Route path='/' Component={LazyHomePage}/>
+          <Route path='/about' Component={LazyAboutPage}/>        
+        </Router>
+      </Suspense>
     </main>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,32 @@
 import image from '../assets/image.png'
 import Link from '../components/Link.jsx'
 
-export default function AboutPage() {
+const i18n = {
+  es:{
+    title: 'Sobre Nosotros',
+    button: 'Volver al Inicio!',
+    description: 'Hola! Me llamo @ppagano y estoy creando un clon de React Router'
+  },
+  en:{
+    title: "About Us",
+    button: 'Go to Home!',
+    description: "Hi! My name is @ppagano and I am creating a clone of React Router"
+  }
+}
+
+const useI18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({routeParams}) {
+    const i18n = useI18n(routeParams.lang ?? 'es')
+  
     return (
       <>
-        <h1>About</h1>
+        <h1>{i18n.title}</h1>
         <div>
-          <p>Desarrollador @ppagano91</p>
-          <Link to="/">Home</Link>
+          <p>{i18n.description}</p>
+          <Link to="/">{i18n.button}</Link>
           <img src={image} alt="Foto de ppagano" />
         </div>
       </>
